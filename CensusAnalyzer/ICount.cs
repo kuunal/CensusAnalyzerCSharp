5f2d60de57cc36e70b59a3494cafe3e993497f24
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-11 02:24:45 +0530

    [Kunal] ADD: interface providing GetCount method to Count class

---
+++ CensusAnalyzer/ICount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyzerProject
+{
+    public interface ICount
+    {
+        public int GetCount(string path, string className);
+    }
+}
