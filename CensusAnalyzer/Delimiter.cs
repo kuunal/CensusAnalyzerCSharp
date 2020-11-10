1db8d0b109731a7ead045c5129ec9bb0ba7e3269
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-11 02:28:07 +0530

    [Kunal] ADD: Delimiter decorator for verifying valid delimiter

---
+++ CensusAnalyzer/Delimiter.cs
@@ -0,0 +1,22 @@
+﻿using CensusAnalyzerProject.Exceptions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyzerProject
+{
+    public class Delimiter : Decorator
+    {
+        public Delimiter(ICensusCSVLoader censusCSVLoader) : base(censusCSVLoader) { }
+        public override ArrayList LoadData(string path, string className)
+        {
+            ArrayList data = base.LoadData(path, className);
+            if (!data[0].ToString().Contains(","))
+            {
+                throw new CensusAnalyzerExceptions(CensusAnalyzerExceptions.ExeptionType.INVALID_DELIMITER);
+            }
+            return data;
+        }
+    }
+}
