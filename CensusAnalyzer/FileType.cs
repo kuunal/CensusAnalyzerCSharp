3b4bd8ee84059e8ab3ee0feb72d40a4f3db29b70
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-11 02:48:41 +0530

    [Kunal] ADD: Decorator for checking type of file

---
+++ CensusAnalyzer/FileType.cs
@@ -0,0 +1,34 @@
+﻿using CensusAnalyzerProject.Exceptions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CensusAnalyzerProject
+{
+    public class FileType : Decorator
+    {
+        const string ALLOWED_EXTENSION = ".csv";
+
+        public FileType(ICensusCSVLoader censusCSVLoader) : base(censusCSVLoader)
+        {
+        }
+
+        public override ArrayList LoadData(string path, string className)
+        {
+            VerifyCSV(path);
+            return base.LoadData(path, className);
+        }
+
+        public void VerifyCSV(string path)
+        {
+            string fileExtension = Path.GetExtension(path);
+            if (!fileExtension.Equals(ALLOWED_EXTENSION))
+            {
+                throw new CensusAnalyzerExceptions(CensusAnalyzerExceptions.ExeptionType.INVALID_FILE);
+            }
+        }
+
+    }
+}
