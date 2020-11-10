af058ed59aaf1355c4657c11342b97f24ec493eb
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-10 23:52:51 +0530

    [Kunal] ADD: Model class for IndianStateCensus

---
+++ CensusAnalyzer/Models/IndianStateCensus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyzerProject.Models
+{
+    class IndianStateCensus
+    {
+        string state { get; set; }
+        long population { get; set; }
+        long areaInSquareKiloMeter { get; set; }
+        long densityPerSquareKiloMeter { get; set; }
+
+    }
+}
