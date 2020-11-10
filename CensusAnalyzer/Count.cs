f7fad798abbd39547b3d285381df387f671faa8c
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-11 02:25:49 +0530

    [Kunal] ADD: Seperate count class for Single Responsiblity

---
+++ CensusAnalyzer/Count.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyzerProject
+{
+    public class Count : Decorator, ICount
+    {
+        public Count(ICensusCSVLoader censusCSVLoader) : base(censusCSVLoader) { }
+
+    
+
+        public override ArrayList LoadData(string path, string className)
+        {
+            return base.LoadData(path, className);
+        }
+
+        public int GetCount(string path, string className)
+        {
+            return LoadData(path, className).Count-1;
+        }
+    }
+}
