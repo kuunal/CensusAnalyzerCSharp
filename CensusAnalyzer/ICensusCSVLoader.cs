a03f93787ceadcba2492d384a80a9969f0544afc
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-11 02:14:38 +0530

    [Kunal] ADD: Interface for census analyzer for decorator design pattern

---
+++ CensusAnalyzer/ICensusCSVLoader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyzerProject
+{
+    public interface ICensusCSVLoader   
+    {
+        public ArrayList LoadData(string path, string className);
+    }
+}
