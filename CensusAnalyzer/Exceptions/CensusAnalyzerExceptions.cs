aca75d9b03e09b905d39a80e35728891f78199b0
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-10 22:52:44 +0530

    [Kunal] ADD: Custom Exception class for CensusAnalyzer

---
+++ CensusAnalyzer/Exceptions/CensusAnalyzerExceptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyzerProject.Exceptions
+{
+    public class CensusAnalyzerExceptions : Exception
+    {
+        public enum ExeptionType{
+            INVALID_FILE,
+            INVALID_TYPE,
+            INVALID_DELIMITER,
+            INVALID_HEADER
+        }
+        public Enum ExceptionType;
+        public CensusAnalyzerExceptions(Enum ExceptionType) : base(ExceptionType.ToString())
+        {
+            this.ExceptionType = ExceptionType;
+        }
+    }
+}
