a058c706ce97b292f1e712f2959c5f14552d5140
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-10 21:15:24 +0530

    [Kunal] ADD: Method for getting count of number of recorrds

--- CensusAnalyzer/CensusAnalyzer.cs
+++ CensusAnalyzer/CensusAnalyzer.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CensusAnalyzerProject
 {
-    class CensusAnalyzer
+    public class CensusAnalyzer
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public int GetCount(string path)
+        {
+            ArrayList CSVData = new ArrayList();
+            if (File.Exists(path))
+            {
+                using (StreamReader streamReader = new StreamReader(path)) { 
+                    while (!streamReader.EndOfStream)
+                    {
+                        CSVData.Add(streamReader.ReadLine());
+                    }
+                }
+            }
+            return CSVData.Count-1;
         }
     }
 }
