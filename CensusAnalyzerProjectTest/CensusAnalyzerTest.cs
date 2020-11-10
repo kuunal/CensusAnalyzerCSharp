732e416f995d045ca21d9410d2995f478f64339b
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-10 21:13:17 +0530

    [Kunal] REFACTOR: Changed CSV file location

--- CensusAnalyzerProjectTest/CensusAnalyzerTest.cs
+++ CensusAnalyzerProjectTest/CensusAnalyzerTest.cs
@@ -6,7 +6,7 @@ namespace CensusAnalyzerProjectTest
 {
     public class CensusAnalyzerTest
     {
-        const string INDIAN_CENSUS_CSV_PATH = "C:/Users/Vishal/source/repos/CensusAnalyzer/CensusAnalyzerTest/utilities/IndiaStateCensusData.csv";
+        const string INDIAN_CENSUS_CSV_PATH = "C:/Users/Vishal/source/repos/CensusAnalyzer/CensusAnalyzerProjectTest/utilities/IndiaStateCensusData.csv";
 
         [SetUp]
         public void Setup()
@@ -18,7 +18,7 @@ namespace CensusAnalyzerProjectTest
         {
             CensusAnalyzer censusAnalyzerObj = new CensusAnalyzer();
             int count = censusAnalyzerObj.GetCount(INDIAN_CENSUS_CSV_PATH);
-            Assert.AreEqual(30, count);
+            Assert.AreEqual(29, count);
         }
     }
 }
