57ed554125c75e30ab1c16f66d153281781d5d97
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-11 02:35:57 +0530

    [Kunal] ADD: Decorator for verifying type of indian census

---
+++ CensusAnalyzer/CSVType.cs
@@ -0,0 +1,32 @@
+﻿using CensusAnalyzerProject.Exceptions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyzerProject
+{
+    class CSVType : Decorator
+    {
+        public CSVType(ICensusCSVLoader censusCSVLoader) : base(censusCSVLoader)
+        {
+        }
+
+        public override ArrayList LoadData(string path, string className)
+        {
+            verifyType("CensusAnalyzerProject.Models." + className);
+            return base.LoadData(path, className);
+        }
+
+        public void verifyType(string className)
+        {
+            Type type = Type.GetType(className);
+            if (type == null)
+            {
+                throw new CensusAnalyzerExceptions(CensusAnalyzerExceptions.ExeptionType.INVALID_TYPE);
+            }
+        }
+
+
+    }
+}
