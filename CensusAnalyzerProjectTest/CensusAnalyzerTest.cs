59cbf56fa02a8e1afc6027fc69858ed2d7adb357
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-10 22:54:45 +0530

    [Kunal] ADD: Test case for wrong file

--- CensusAnalyzerProjectTest/CensusAnalyzerTest.cs
+++ CensusAnalyzerProjectTest/CensusAnalyzerTest.cs
@@ -1,24 +1,40 @@
 using CensusAnalyzerProject;
 using NUnit.Framework;
-
+using CensusAnalyzerProject.Exceptions;
+using System;
+using System.IO;
 
 namespace CensusAnalyzerProjectTest
 {
     public class CensusAnalyzerTest
     {
-        const string INDIAN_CENSUS_CSV_PATH = "C:/Users/Vishal/source/repos/CensusAnalyzer/CensusAnalyzerProjectTest/utilities/IndiaStateCensusData.csv";
-
+        const string INDIAN_CENSUS_CSV_PATH = "C:/Users/Vishal/source/repos/CensusAnalyzer/CensusAnalyzerProjectTest/Utilities/IndiaStateCensusData.csv";
+        const string WRONG_INDIAN_CENSUS_CSV_PATH = "C:/Users/Vishal/source/repos/CensusAnalyzer/CensusAnalyzerProjectTest/Utilities/IndiaStateCensusData.txt";
+        CensusAnalyzer censusAnalyzerObj;
         [SetUp]
         public void Setup()
         {
+            censusAnalyzerObj = new CensusAnalyzer();
         }
 
         [Test]
         public void givenIndianCensusCSV_WhenCorrect_ReturnsNumberOfRecords()
         {
-            CensusAnalyzer censusAnalyzerObj = new CensusAnalyzer();
             int count = censusAnalyzerObj.GetCount(INDIAN_CENSUS_CSV_PATH);
             Assert.AreEqual(29, count);
+        }
+
+        [Test]  
+        public void givenIndianCensusCSV_WhenIncorrect_ReturnsException()
+        {
+            try
+            {
+                censusAnalyzerObj.VerifyCSV(WRONG_INDIAN_CENSUS_CSV_PATH);
+            }
+            catch (CensusAnalyzerExceptions e)
+            {
+                Assert.AreEqual(CensusAnalyzerExceptions.ExeptionType.INVALID_FILE, e.ExceptionType);
+            }
         }
     }
 }
