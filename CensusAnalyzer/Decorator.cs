877474916e27584c1e13bc863decd0695cb59598
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-11 02:23:15 +0530

    [Kunal] ADD: base decorator class

---
+++ CensusAnalyzer/Decorator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyzerProject
+{
+    public class Decorator : ICensusCSVLoader
+    {
+        private ICensusCSVLoader censusCSVLoader;
+
+        public Decorator(ICensusCSVLoader censusCSVLoader)
+        {
+            this.censusCSVLoader = censusCSVLoader;
+        }
+        public virtual ArrayList LoadData(string path, string className)
+        {
+            return censusCSVLoader.LoadData(path, className);
+        }
+
+    }
+}
