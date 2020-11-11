80048b5330948197fe47c6df3c9681f3e0c35b1c
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-11 09:51:30 +0530

    [Kunal] ADD: Model for Indian state code

---
+++ CensusAnalyzer/Models/IndianStateCode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyzerProject.Models
+{
+    class IndianStateCode
+    {
+        string stateName { get; set; }
+        string stateCode { get; set; }
+        int tin { get; set; }
+    }
+}
