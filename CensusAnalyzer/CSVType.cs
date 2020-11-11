77086c68d8b4095fbb61264a4c5d3120a0620375
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-11 09:22:03 +0530

    [Kunal] REFACTOR: Made class public for testing

--- CensusAnalyzer/CSVType.cs
+++ CensusAnalyzer/CSVType.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace CensusAnalyzerProject
 {
-    class CSVType : Decorator
+    public class CSVType : Decorator
     {
         public CSVType(ICensusCSVLoader censusCSVLoader) : base(censusCSVLoader)
         {
---
+++ CensusAnalyzer/Header.cs
@@ -0,0 +1,38 @@
+﻿using CensusAnalyzerProject.Exceptions;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace CensusAnalyzerProject
+{
+    public class Header : Decorator
+    {
+        public Header(ICensusCSVLoader censusCSVLoader) : base(censusCSVLoader)
+        {
+        }
+
+        public override ArrayList LoadData(string path, string className)
+        {
+            ArrayList data = base.LoadData(path, className);
+            ValidateHeaders(data[0].ToString(), className);
+            return data;
+        }
+
+        public void ValidateHeaders(string headers, string className)
+        {
+            string[] header = headers.Split(",");
+            Type type = Type.GetType("CensusAnalyzerProject.Models." + className);
+            PropertyInfo[] properties = type.GetProperties();
+            foreach(PropertyInfo property in properties)
+            {
+                if (!Array.Exists<string>(header, rawHeaders => rawHeaders.Equals(property)))
+                {
+                    throw new CensusAnalyzerExceptions(CensusAnalyzerExceptions.ExeptionType.INVALID_HEADER);
+                }
+            }
+        }
+
+    }
+}
