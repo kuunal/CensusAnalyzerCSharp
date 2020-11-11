5c25b630494292cc8c57036f5b40fcd4a0548cf8
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-12 00:27:30 +0530

    [Kunal] ADD: Interface for sort

---
+++ CensusAnalyzer/ISort.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyzerProject
+{
+    public interface ISort
+    {
+        public ArrayList sort(ArrayList data, string field);
+    }
+}
