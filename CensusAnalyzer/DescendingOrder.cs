3bebc69200c4805f144146ae6c8b90c7387cf4b3
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-12 19:28:39 +0530

    [Kunal] ADD: Logic for sorting in descending

---
+++ CensusAnalyzer/DescendingOrder.cs
@@ -0,0 +1,18 @@
+﻿using CensusAnalyzerProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CensusAnalyzerProject
+{
+    public class DescendingOrder : ISort
+    {
+        public List<CensusDAO> sort(List<CensusDAO> data, string field)
+        {
+            Type type = typeof(CensusDAO);
+            var fields = type.GetField(field);
+            return data.OrderByDescending(row => fields.GetValue(row)).ToList();
+        }
+    }
+}
