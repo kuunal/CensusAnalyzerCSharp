3fd2a77858d3a4344f3f319a588ca87bd47e51fc
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-12 23:40:18 +0530

    [kunal] ADD: DTO for USCensus

---
+++ CensusAnalyzer/DTO/USCensusDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyzerProject.DTO
+{
+    public class USCensusDTO
+    {
+        public string state;
+        public long population;
+        public double area;
+        public double density;
+
+
+        public USCensusDTO(string[] values)
+        {
+            this.state = values[1];
+            this.population = long.Parse(values[2]);
+            this.area = double.Parse(values[4]);
+            this.density = double.Parse(values[7]);
+        }
+    }
+
+}
