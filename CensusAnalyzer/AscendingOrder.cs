fb2b96db84000ba60114d9e55130635f72e2ac37
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-12 19:28:06 +0530

    [Kunal] ADD: Logic for sorting in asc order

---
+++ CensusAnalyzer/AscendingOrder.cs
@@ -0,0 +1,19 @@
+﻿using CensusAnalyzerProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CensusAnalyzerProject
+{
+    public class AscendingOrder : ISort
+    {
+        public List<CensusDAO> sort(List<CensusDAO> data, string field)
+        {
+            Type type = typeof(CensusDAO);
+            var fields = type.GetField(field);
+            return data.OrderBy(row => fields.GetValue(row)).ToList();
+        }
+    }
+}
