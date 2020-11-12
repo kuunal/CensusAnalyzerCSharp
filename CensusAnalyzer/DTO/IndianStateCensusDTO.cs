e42b4c36e6553f50afbce297fb3d899c07bf927b
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-12 15:31:56 +0530

    [Kunal] ADD: DTO for indian census and state

---
+++ CensusAnalyzer/DTO/IndianStateCensusDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyzerProject.DTO
+{
+    public class IndianStateCensusDTO
+    {
+        public string state;
+        public long population;
+        public long areaInSquareKiloMeter;
+        public long densityPerSquareKiloMeter;
+        
+        public IndianStateCensusDTO(string[] values)
+        {
+            this.state = values[0];
+            this.areaInSquareKiloMeter = long.Parse(values[1]);
+            this.population = long.Parse(values[2]);
+            this.densityPerSquareKiloMeter = long.Parse(values[3]);
+        }
+    }
+}
---
+++ CensusAnalyzer/DTO/IndianStateCodeDTO.cs
@@ -0,0 +1,24 @@
+﻿using CensusAnalyzerProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyzerProject.DTO
+{
+    public class IndianStateCodeDTO
+    {
+        public int srNo;
+        public string stateName;
+        public string stateCode;
+        public int tIN; 
+        public IndianStateCodeDTO(string[] values)
+        {
+            this.srNo = Convert.ToInt32(values[0]);
+            this.stateCode = values[3];
+            this.stateName = values[1];
+            this.tIN = Convert.ToInt32(values[2]);
+        }
+
+
+    }
+}
