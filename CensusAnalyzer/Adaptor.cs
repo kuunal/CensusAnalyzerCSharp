3af93dd32c600214d6f521e336518aecb2310e85
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-13 23:54:43 +0530

    [Kunal] ADD: Implemented adaptor pattern

---
+++ CensusAnalyzer/Adaptor.cs
@@ -0,0 +1,34 @@
+﻿using CensusAnalyzerProject.DTO;
+using CensusAnalyzerProject.Exceptions;
+using CensusAnalyzerProject.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CensusAnalyzerProject
+{
+    public class Adaptor : Loader, ICensusCSVLoader
+    {
+        static Dictionary<string, List<CensusDAO>> dict = new Dictionary<string, List<CensusDAO>>();
+
+        public new Dictionary<string, List<CensusDAO>> LoadData(string path, string className)
+        {
+            string[] rows= base.LoadData(path, className);
+            Factory factory = new Factory();
+            CountryAdaptor adaptorObj = factory.GetCountryCensus(className);
+            Dictionary<string, List<CensusDAO>> censusDict = adaptorObj.CSVParser(rows, className);
+            try
+            {
+                dict.Add(className, censusDict[className]);
+            }
+            catch (System.ArgumentException)
+            {
+                dict[className] = censusDict[className];
+            }
+            return dict;
+        }
+    }
+}
+    
