2f8c59752cb25e5b99f5a2198257b74e42b3325c
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-14 00:51:59 +0530

    [Kunal] ADD: path field for storing path for particular object

--- CensusAnalyzer/Services.cs
+++ CensusAnalyzer/Services.cs
@@ -13,12 +13,14 @@ namespace CensusAnalyzerProject
     public class Services : Decorator, ICount
     {
         const string JSON_FILE_PATH = "C:/Users/Vishal/source/repos/CensusAnalyzer/CensusAnalyzerProjectTest/utilities/JSONOP.json";
+        string path;
         public Services(ICensusCSVLoader censusCSVLoader) : base(censusCSVLoader) { }
 
     
 
         public override Dictionary<string, List<CensusDAO>> LoadData(string path, string className)
         {
+            this.path = path;
             return base.LoadData(path, className);
         }
 
@@ -27,10 +29,11 @@ namespace CensusAnalyzerProject
             return LoadData(path, className)[className].Count;
         }
 
-        public List<CensusDAO> SortData(Dictionary<string, List<CensusDAO>> dict, string field, string className, CustomEnums.sort sorttype)
+        public List<CensusDAO> SortData(string field, string className, CustomEnums.sort sorttype)
         {
             Factory factory = new Factory();
             ISort sortObj = factory.GetSort(sorttype);
+            Dictionary<string, List<CensusDAO>> dict = LoadData(path, className);
             List<CensusDAO> list = dict[className];
             List<CensusDAO> sortedList = sortObj.sort(list, field);
             if (CustomEnums.sort.DESCENDING.Equals(sorttype))
