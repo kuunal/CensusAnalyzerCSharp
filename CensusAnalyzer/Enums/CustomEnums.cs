ee108645dcdedb23db968b9812fd99415e3dd360
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-15 16:58:16 +0530

    [Kunal] ADD: Enums for country

--- CensusAnalyzer/Enums/CustomEnums.cs
+++ CensusAnalyzer/Enums/CustomEnums.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace CensusAnalyzerProject.Enums
@@ -8,6 +9,42 @@ namespace CensusAnalyzerProject.Enums
     {
         public enum sort { 
         ASCENDING, DESCENDING
+        }
+
+        public enum TYPE
+        {
+            [Description("USCensusDTO")]
+            USCENSUS,
+            [Description("IndianStateCodeDTO")]
+            INDIASTATECODE,
+            [Description("IndianStateCensusDTO")]
+            INDIASTATECENSUS,
+            [Description("wrong")]
+            WRONGTYPE
+        }
+
+        public enum INDIA_STATE_CENSUS_FIELD
+        {
+            [Description("state")] 
+            STATE,
+
+            [Description("population")]
+            POPULATION,
+
+            [Description("areaInSquareKiloMeter")]
+            AREA,
+
+            [Description("densityPerSquareKiloMeter")]
+            DENSITY,
+
+            [Description("srNo")]
+            SRNO,
+
+            [Description("tIN")]
+            TIN,
+
+            [Description("stateCode")]
+            STATECODE
         }
     }
 }
